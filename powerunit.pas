f8ed0427f434a62371097bc340c1a0c7b0879533
NoUrEdDiN <noureddin95@gmail.com>
2013-01-01 21:13:25 +0200

    Update - Tue 01/01/2013 21:13

--- powerunit.pas
+++ powerunit.pas
@@ -16,8 +16,8 @@ implementation
 procedure Power(action: string);
 begin
   Case lowercase(action) of
-    'logout': fpsystem('(gnome-session-save --kill --silent || mate-session-save --kill --silent) || killall -u $USER');
-    'lockscreen': fpsystem('for i in `ls /usr/bin/*screensaver`; do if [ ! -z `pidof $i` ] ; then ${i}-command -l ; fi ;done');
+    'logout': fpsystem('(gnome-session-save --kill --silent || mate-session-save --kill --silent) && killall -u $USER');
+    'lockscreen': fpsystem('for i in `ls /usr/bin/*screensaver`; do if [ ! -z `pidof $i` ] && [ -e "${i}-command" ] ; then ${i}-command -l ; fi ; done');
     'switchuser': fpsystem('dm-tool switch-to-greeter||mdmflexiserver||gdmflexiserver');
     'shutdown': fpsystem('dbus-send --system --print-reply --dest=org.freedesktop.ConsoleKit /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Stop');
     'restart': fpsystem('dbus-send --system --print-reply --dest=org.freedesktop.ConsoleKit /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Restart');
@@ -33,7 +33,7 @@ begin
   Spt[1]:=True;
   if fpsystem('for i in `ls /usr/sbin/*dm`; do if [ ! -z "`pidof $i`" ] ; then case $i in /usr/sbin/lightdm);;/usr/sbin/mdm);;/usr/sbin/gdm);;*)exit 2;;esac;fi;done') = 0 then
      Spt[2]:=True;
-  if fpsystem('for i in `ls /usr/bin/*screensaver`; do if [ ! -z `pidof $i` ] ; then if [ ! -e "${i}-command" ] ; then exit 2 ;fi;fi;done') = 0 then
+  if fpsystem('a=''''; for i in `ls /usr/bin/*screensaver`; do if [ -n `pidof $i` ] && [ -e "${i}-command" ]; then a=1; fi; done; if [ -z "$a" ]; then exit 2; fi') = 0 then
      Spt[3]:=True;
   Spt[4]:=True;
   Spt[5]:=True;
